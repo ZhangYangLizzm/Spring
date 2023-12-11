17c2330d0179a267ac5f4efac0aeed7b254482ed
张殃离 <96040653+ZhangYangLizzm@users.noreply.github.com>
2023-12-11 22:41:25 +0800

    spring初始化

--- src/main/java/com/heima/service/UserService.java
+++ src/main/java/com/heima/service/UserService.java
@@ -3,9 +3,9 @@ package com.heima.service;
 import com.heima.pojo.User;
 
 public interface UserService {
-    public User findUserById(Integer id);
+    User findUserById(Integer id);
 
-    public void createUser(User user);
+    void createUser(User user);
 
-    public void deleteUserById(Integer id);
+    void deleteUserById(Integer id);
 }
